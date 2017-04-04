ad5e98312fabf91f3c4dc66e7d056d38c0abe10d
Justin-MM <justinmacharia78@gmail.com>
2017-04-04 16:29:29 +0300

    OOP prototype concepts added

---
+++ Day 2/OOP/OOP_concepts.py
@@ -0,0 +1,45 @@
+
+class Cups (object):
+  def __init__ (self, color, size, make):
+    self.color = color
+    self.size = size
+    self.make = make
+
+# class Mugs inherits from Cups
+class Mugs (Mugs):
+
+  def __init__ (self, color, size, make):
+    super (ChinaMugs, self).__init__(color, size, make)
+
+  def purchase_mug(self, make):
+    if self.make == 'China Mugs':
+      return "Price is 250 Kenya Shillings"
+    elif self.make == 'Porcelain':
+      return "Price is 200 Kenya Shillings"
+    else:
+      return "Out of stock!"
+
+  def sizes_available(self, size):
+    if self.size == "small":
+      return "Out of stock"
+    elif self.size == "Big":
+      return "Mugs are available"
+    else:
+      return "Medium sizes coming soon"
+
+  def mug_colors (self, color):
+    colors_available = ["red", "black", "white"]
+    if self.color not in colors_available:
+      return "Invalid color"
+    else:
+      return "Color is available"
+
+
+
+inventory = Cups ('Black', "Big", 'new')
+purchase_Mug = Mugs ('black', "Big", 'China Mugs')
+
+print (inventory)
+print (purchase_Mug)
+
+__author__ = 'Justin M'
