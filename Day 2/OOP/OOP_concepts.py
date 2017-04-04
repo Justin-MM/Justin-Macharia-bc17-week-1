e28c178739f8f2124bb8d1b1d75277b1c55716fa
Justin-MM <justinmacharia78@gmail.com>
2017-04-04 16:57:41 +0300

    Added comments and code to show other OOP aspects

--- Day 2/OOP/OOP_concepts.py
+++ Day 2/OOP/OOP_concepts.py
@@ -1,9 +1,24 @@
-
+# OOP concepts used include inheritance , Polymorphism and encapsulation
 class Cups (object):
   def __init__ (self, color, size, make):
     self.color = color
     self.size = size
     self.make = make
+
+  def purchase_cup(self, make):
+    if self.make == 'Tea cup':
+      return "Price is 50 Kenya Shillings"
+    elif self.make == 'Porcelain':
+      return "Price is 70 Kenya Shillings"
+    else:
+      return "Out of stock!"
+
+      # protected function that cannot be accessed outside of the class
+  def __source_info(self, source):
+    if self.source == "China":
+        return "GangZhou Industries"
+    else:
+        return "That is a royalty free item"
 
 # class Mugs inherits from Cups
 class Mugs (Cups):
@@ -11,7 +26,8 @@ class Mugs (Cups):
   def __init__ (self, color, size, make):
     super (Mugs, self).__init__(color, size, make)
 
-  def purchase_mug(self, make):
+    # polymorphic function with varying implementation from the above Cup class
+  def purchase_cup(self, make):
     if self.make == 'China Mugs':
       return "Price is 250 Kenya Shillings"
     elif self.make == 'Porcelain':
@@ -32,7 +48,7 @@ class Mugs (Cups):
     if self.color not in colors_available:
       return "Invalid color"
     else:
-      return "Color is available"
+      return " That Color is available"
 
 
 
@@ -40,6 +56,6 @@ inventory = Cups ('Black', "Big", 'new')
 purchase_Mug = Mugs ('black', "Big", 'China Mugs')
 
 
-print (purchase_Mug.purchase_mug("China Mug"))
+print (purchase_Mug.purchase_cup("China Mug"))
 
 __author__ = 'Justin M'
