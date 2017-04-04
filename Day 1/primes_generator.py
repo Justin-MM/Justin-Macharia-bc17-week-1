175485936fa79a78ef308fa1b9810cb18cbfd8af
Justin-MM <justinmacharia78@gmail.com>
2017-04-04 03:38:44 +0300

    Added asymptotic analysis to the prime function

--- Day 1/primes_generator.py
+++ Day 1/primes_generator.py
@@ -14,3 +14,10 @@ def prime_function(n):
     except TypeError:
         raise TypeError("The provided input is not an integer!")
     return prime_list
+
+# The first seven lines leading up to the for loop for a constant execution time.
+# This means that regardless of the size of n, the execution time of those lines of code does not increase
+# The For loop in my function runs n-2 times and the code therein increases in execution time by the size of n.
+# The larger the size of n, the longer it takes. The Execution times increases linearly.
+# Other code outside the for loop has constant execution time per statement
+# Hence the complexity of our function can be said to be O(n) as time increases directly proportional to input.
